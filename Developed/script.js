61b1da90dd4a98302c042570fd372f5e760e3724
Sean Ashby <sean.r.ashby@gmail.com>
2021-11-17 22:06:16 -0800

    updated a vars and arrays, added some functionality, deleted vs code folder that randomly appeared, uninstalled and reinstalled command path in VS code

--- Developed/script.js
+++ Developed/script.js
@@ -1,12 +1,67 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+
+// keep adding symbols 
+var symbols = ["!", ];
+
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+// make sure to create uppercase letters also 
+
+var upperMaker = function(x) {
+  return x.toUpperCase();
+
+}; 
+var bigLetters = letters.map(upperMaker);
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
+  var passwordLength; 
+  
+  var specialChar; 
+
+  var upperCase; 
+
+  var lowerCase; 
+
+  var numerals; 
+  
+
   passwordText.value = password;
+
+  passwordLength = parseInt(prompt("Enter a number between 8 and 128"));
+
+  if( !passwordLength ){
+    alert("Choose a number of characters between 8 and 128")
+
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    alert("Choose a number of characters between 8 and 128");
+    return; 
+  } else {
+    specialChar = confirm("Do you want special characters?");
+
+    upperCase = confirm("Do you want upper case letters?");
+
+    lowerCase = confirm("Do you want lower case letters?");
+
+    numerals = confirm("Do you want numbers?");
+
+  }; 
+
+  for (var i = 0; i < passwordLength; i++) {
+    // have to change characters.length 
+    var randomNumber = Math.floor(Math.random() * characters.length);
+    password + characters.substring(randomNumber, randomNumber +1);
+  }
+  
+  
 
 }
 
