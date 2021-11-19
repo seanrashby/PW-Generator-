56c72d813d09192e990eb1f8fab1e350e84302a1
Sean Ashby <sean.r.ashby@gmail.com>
2021-11-19 15:05:11 -0800

    created an object with multiple arrays to reference in my for loop, updated the prompts and alerts, trouble shot the prompts after creating my for loop which required multiple updates to delete variables from the array given user choice

--- Developed/script.js
+++ Developed/script.js
@@ -1,25 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var numbers = [0,1,2,3,4,5,6,7,8,9];
+var object = {
+  numbers: [0,1,2,3,4,5,6,7,8,9],
 
-// keep adding symbols 
-var symbols = ["!", ];
+  symbols: ["!", "#", "$", "%","&","(",")","*", "-"],
 
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  letters: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+}
 
-// make sure to create uppercase letters also 
-
-var upperMaker = function(x) {
-  return x.toUpperCase();
-
-}; 
-var bigLetters = letters.map(upperMaker);
-
-
-// Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
+  var password = "";
 
   var passwordText = document.querySelector("#password");
 
@@ -33,9 +24,6 @@ function writePassword() {
 
   var numerals; 
   
-
-  passwordText.value = password;
-
   passwordLength = parseInt(prompt("Enter a number between 8 and 128"));
 
   if( !passwordLength ){
@@ -45,23 +33,57 @@ function writePassword() {
     alert("Choose a number of characters between 8 and 128");
     return; 
   } else {
+
     specialChar = confirm("Do you want special characters?");
+
+      if(!specialChar){
+        delete object.symbols;
+      }
 
     upperCase = confirm("Do you want upper case letters?");
 
+      if(upperCase){
+        var upperMaker = function(x) {
+
+          return x.toUpperCase();
+        
+        }; 
+        object.letters = object.letters.map(upperMaker);
+      }
+
     lowerCase = confirm("Do you want lower case letters?");
+      if(lowerCase){
+
+        var lowerMaker = function(x){
+
+          return x.toLowerCase();
+        }
+      }
 
     numerals = confirm("Do you want numbers?");
+      if(!numerals){
 
-  }; 
+        delete object.numbers;
+      }
+
+  };
+  
 
   for (var i = 0; i < passwordLength; i++) {
-    // have to change characters.length 
-    var randomNumber = Math.floor(Math.random() * characters.length);
-    password + characters.substring(randomNumber, randomNumber +1);
+     
+    var keys = Object.keys(object);
+
+    var randomKey = Math.floor(Math.random() * keys.length)
+        
+    var key = keys[randomKey]
+
+    var randomNumber = Math.floor(Math.random() * object[key].length);
+
+    password += object[key][randomNumber];
+    
   }
-  
-  
+  passwordText.value = password;
+
 
 }
 
